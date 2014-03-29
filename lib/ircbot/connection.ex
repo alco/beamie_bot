e9eff0a1c88056da0c5af6432f923064342c0f09
Alexei Sholik <alcosholik@gmail.com>
2014-03-29 13:27:37 +0200

    Augment link scanning hook to also show docs

--- lib/ircbot/connection.ex
+++ lib/ircbot/connection.ex
@@ -1,7 +1,7 @@
 defmodule IRCBot.Connection do
   @nickname "beamie"
-  #@channel "exligir"
-  @channel "elixir-lang"
+  @channel "exligir"
+  #@channel "elixir-lang"
 
   defrecordp :hookrec, [type: nil, direct: false, fn: nil]
   defrecord State, hooks: []
@@ -366,10 +366,43 @@ end
 
 defmodule LinkScanHook do
   def run(sender, text) do
+    mid_frag = "[A-Z][[:alnum:]_]*"
+    mid = "#{mid_frag}(?:\.#{mid_frag})*"
+    fid = "[^A-Z](?:[^/[:space:].]|/(?!\\d))*"
+
+    module_re = ~r"(?<=&|^| )#{mid}(?=~)"
+    fun_re    = ~r"(?<=&|^| )#{fid}(?:/(\d))?(?=~)"
+    mfa_re    = ~r"(?<=&|^| )#{mid}\.#{fid}(?:/\d)?(?=~)"
+
+    (
     Regex.scan(~r"\b([-_[:alnum:]]+)~", text)
-    |> Enum.map(fn [_, word] ->
+    |> mapf(fn [_, word] ->
          LinkHook.run(sender, word)
        end)
+    ) ++ (
+    Regex.scan(module_re, text)
+    |> flatmapf(fn [thing] ->
+         DocHook.run(sender, "doc " <> thing)
+       end)
+    ) ++ (
+    Regex.scan(fun_re, text)
+    |> flatmapf(fn [thing] ->
+         DocHook.run(sender, "doc " <> thing)
+       end)
+    ) ++ (
+    Regex.scan(mfa_re, text)
+    |> flatmapf(fn [thing] ->
+         DocHook.run(sender, "doc " <> thing)
+       end)
+    )
+  end
+
+  defp mapf(c, f) do
+    Enum.map(c, f) |> Enum.reject(&(nil == &1))
+  end
+
+  defp flatmapf(c, f) do
+    Enum.flat_map(c, f) |> Enum.reject(&(nil == &1))
   end
 end
 
