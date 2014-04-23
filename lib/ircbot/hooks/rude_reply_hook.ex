c58994adeb6713451e703a4418ec53736e3859c6
Alexei Sholik <alcosholik@gmail.com>
2014-04-23 21:12:24 +0300

    Diversify RudeReplyHook

--- lib/ircbot/hooks/rude_reply_hook.ex
+++ lib/ircbot/hooks/rude_reply_hook.ex
@@ -1,15 +1,29 @@
 defmodule RudeReplyHook do
+  @erlang_replies [
+    "ask nox about it", "erlang? never heard of it", "erlang? what erlang?",
+    "erlang? did you mean prolog?", "my lang is not yerlang",
+  ]
+  @num_replies Enum.count(@erlang_replies)
+
   def run(sender, text) do
     downtext = String.downcase(text)
-    if String.ends_with?(downtext, "?") do
-      if String.contains?(downtext, ["homoiconic", "erlang"]) do
-        if sender == "nox" do
-          {:reply, sender, "haha, funny"}
-        else
+    if String.contains?(downtext, "?") do
+      cond do
+        String.contains?(downtext, "homoiconic") ->
           {:reply, sender, "ask nox about it"}
-        end
-      else
-        {:reply, sender, "I don't know. Perhaps you should google it"}
+
+        String.contains?(downtext, "elixir") ->
+          {:reply, sender, "Elixir is the best language there is"}
+
+        String.contains?(downtext, ["homoiconic", "erlang"]) ->
+          if sender == "nox" do
+            {:reply, sender, "haha, funny"}
+          else
+            {:reply, sender, Enum.at(@erlang_replies, :random.uniform(@num_replies)-1)}
+          end
+
+        true ->
+          {:reply, sender, "I don't know. Perhaps you should google it"}
       end
     else
       {:reply, sender, "I don't like you either"}
