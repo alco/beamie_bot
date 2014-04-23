ca80a35e68053af8edc8bab8f13c298434c8018b
Alexei Sholik <alcosholik@gmail.com>
2014-04-23 21:04:22 +0300

    Diversify PingHook's replies

--- lib/ircbot/hooks/ping_hook.ex
+++ lib/ircbot/hooks/ping_hook.ex
@@ -1,7 +1,12 @@
 defmodule PingHook do
+  @replies [
+    "pong", "shpunk", "spank", "pork", "dong",
+  ]
+  @num_replies Enum.count(@replies)
+
   def run(sender, text) do
     if String.downcase(text) == "ping" do
-      {:reply, sender, "pong"}
+      {:reply, sender, Enum.at(@replies, :random.uniform(@num_replies)-1)}
     end
   end
 end
