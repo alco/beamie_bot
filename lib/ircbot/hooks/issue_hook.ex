85282bcf24e2c449b792692cf4bee0494abd8a4a
Alexei Sholik <alcosholik@gmail.com>
2014-06-23 03:23:52 +0300

    Search for issues on friend projects

--- lib/ircbot/hooks/issue_hook.ex
+++ lib/ircbot/hooks/issue_hook.ex
@@ -1,13 +1,17 @@
 defmodule IssueHook do
   def run(_sender, text) do
     IO.puts "Testing text for issues: '#{text}'"
-    Regex.scan(~r"(?: |^)#(\d+)(?:(?=[[:space:]])|$)|issue[[:space:]]+#?(\d+)(?:(?=[[:space:]])|$)", text)
-    |> Enum.map(fn x ->
-         [_, num] = Enum.reject(x, &match?("", &1))
-         num
-       end)
+    Regex.scan(~r"(?: |^|)([[:alpha:]]+)?#(\d+)(?:(?=[[:space:]])|$)|issue[[:space:]]+#?(\d+)(?:(?=[[:space:]])|$)", text)
+    |> Enum.map(fn [_, proj, num] -> {proj, num} end)
     #|> pfilter(&issue_valid?/1)
-    |> Enum.map(fn x -> {:msg, "https://github.com/elixir-lang/elixir/issues/#{x}"} end)
+    |> Enum.map(fn
+      {"", num} -> {:msg, "https://github.com/elixir-lang/elixir/issues/#{num}"}
+      {"hex", num} -> {:msg, "https://github.com/ericmj/hex/issues/#{num}"}
+      {"plug", num} -> {:msg, "https://github.com/elixir-lang/plug/issues/#{num}"}
+      {"doc", num} -> {:msg, "https://github.com/elixir-lang/ex_doc/issues/#{num}"}
+      {"site", num} -> {:msg, "https://github.com/elixir-lang/elixir-lang.github.com/issues/#{num}"}
+      _ -> nil
+    end)
   end
 
   defp issue_valid?(num) do
@@ -27,7 +31,7 @@ defmodule IssueHook do
     #IO.puts "Filtering #{inspect coll}"
     parent = self()
     Enum.map(coll, fn elem ->
-      spawn(fn -> Process.send(parent, {self(), elem, f.(elem)}) end)
+      spawn(fn -> send(parent, {self(), elem, f.(elem)}) end)
     end) |> collect_replies
   end
 
