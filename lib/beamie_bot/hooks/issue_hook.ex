061fcff2d720ab404e8e115aa3b1a4bb25a004ed
Alexei Sholik <alcosholik@gmail.com>
2014-08-25 18:26:26 +0300

    Comment out unused code in IssueHook

--- lib/beamie_bot/hooks/issue_hook.ex
+++ lib/beamie_bot/hooks/issue_hook.ex
@@ -24,48 +24,48 @@ defmodule IssueHook do
     end)
   end
 
-  defp issue_valid?(num) do
-    url_valid?('https://api.github.com/repos/elixir-lang/elixir/issues/#{num}')
-  end
-
-  defp url_valid?(url) do
-    case :httpc.request(:head, {url, [{'User-Agent', 'httpc'}]}, [], []) do
-      {:ok, {{_, status, _response}, _headers, _}} ->
-        status != 404
-      _ -> nil
-    end
-  end
-
-
-  defp pfilter(coll, f) do
-    #IO.puts "Filtering #{inspect coll}"
-    parent = self()
-    Enum.map(coll, fn elem ->
-      spawn(fn -> send(parent, {self(), elem, f.(elem)}) end)
-    end) |> collect_replies
-  end
-
-  defp collect_replies(pids) do
-    collect_replies(pids, [])
-  end
-
-  defp collect_replies([], acc) do
-    Enum.reverse(acc)
-  end
-
-  defp collect_replies(pids, acc) do
-    receive do
-      {pid, elem, reply} ->
-        #IO.puts "Got reply: #{inspect elem} #{inspect reply}"
-        acc = if reply do
-          [elem|acc]
-        else
-          acc
-        end
-        collect_replies(List.delete(pids, pid), acc)
-    after 1000 ->
-      #IO.puts "Timout"
-      []
-    end
-  end
+  #  defp issue_valid?(num) do
+  #    url_valid?('https://api.github.com/repos/elixir-lang/elixir/issues/#{num}')
+  #  end
+  #
+  #  defp url_valid?(url) do
+  #    case :httpc.request(:head, {url, [{'User-Agent', 'httpc'}]}, [], []) do
+  #      {:ok, {{_, status, _response}, _headers, _}} ->
+  #        status != 404
+  #      _ -> nil
+  #    end
+  #  end
+  #
+  #
+  #  defp pfilter(coll, f) do
+  #    #IO.puts "Filtering #{inspect coll}"
+  #    parent = self()
+  #    Enum.map(coll, fn elem ->
+  #      spawn(fn -> send(parent, {self(), elem, f.(elem)}) end)
+  #    end) |> collect_replies
+  #  end
+  #
+  #  defp collect_replies(pids) do
+  #    collect_replies(pids, [])
+  #  end
+  #
+  #  defp collect_replies([], acc) do
+  #    Enum.reverse(acc)
+  #  end
+  #
+  #  defp collect_replies(pids, acc) do
+  #    receive do
+  #      {pid, elem, reply} ->
+  #        #IO.puts "Got reply: #{inspect elem} #{inspect reply}"
+  #        acc = if reply do
+  #          [elem|acc]
+  #        else
+  #          acc
+  #        end
+  #        collect_replies(List.delete(pids, pid), acc)
+  #    after 1000 ->
+  #      #IO.puts "Timout"
+  #      []
+  #    end
+  #  end
 end
